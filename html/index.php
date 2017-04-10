00d2ca4a1ad6862753806129b033b481e8355121
Bombdude <bombdude@gmail.com>
2017-04-10 18:20:44 -0400

    Update index.php

--- html/index.php
+++ html/index.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 
-<h1>Welcome to Princess Productions</h1><br>
+<h1>Welcome to A Princess Productions Music Co!</h1><br>
 <p style="font-size:30px">This is a website in progress, this is a preview to allow our customers to browse and search the karaoke catalog...
 <br>
 <a href="songlist.php">Search for karaoke songs...</a></p>
